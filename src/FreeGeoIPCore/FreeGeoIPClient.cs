7d73ca38b42edde602e2a6dbf2a2deae6187fcce
mattosaurus <mattosaurus@outlook.com>
2017-02-09 12:50:00 +0000

    Creation

---
+++ src/FreeGeoIPCore/FreeGeoIPClient.cs
@@ -0,0 +1,28 @@
+﻿using FreeGeoIPCore.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FreeGeoIPCore
+{
+    public class FreeGeoIPClient
+    {
+        public async Task<Location> GetLocation(string ipAddress)
+        {
+            string url = "https://freegeoip.net/json/";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(new Uri(url));
+
+                response.EnsureSuccessStatusCode();
+                Location location = JsonConvert.DeserializeObject<Location>(await response.Content.ReadAsStringAsync());
+
+                return location;
+            }
+        }
+    }
+}
---
+++ src/FreeGeoIPCore/Models/Location.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeGeoIPCore.Models
+{
+    public class Location
+    {
+        [JsonProperty("ip")]
+        public string Ip { get; set; }
+
+        [JsonProperty("country_code")]
+        public string CountryCode { get; set; }
+
+        [JsonProperty("country_name")]
+        public string CountryName { get; set; }
+
+        [JsonProperty("region_code")]
+        public string RegionCode { get; set; }
+
+        [JsonProperty("region_name")]
+        public string RegionName { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("zip_code")]
+        public string ZipCode { get; set; }
+
+        [JsonProperty("time_zone")]
+        public string TimeZone { get; set; }
+
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("metro_code")]
+        public string MetroCode { get; set; }
+    }
+}
