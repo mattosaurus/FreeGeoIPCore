fb8661fd6ccd756db8f9874f5449640c56b7f6da
mattosaurus <mattosaurus@outlook.com>
2017-02-17 12:47:01 +0000

    Added overload method that actually uses supplied IP address.

---
+++ src/FreeGeoIPCore/AppCode/Common.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace FreeGeoIPCore.AppCode
+{
+    public static class Common
+    {
+        public static string GetRequestIP(IHttpContextAccessor httpContextAccessor, bool tryUseXForwardHeader = true)
+        {
+            string ip = null;
+
+            if (tryUseXForwardHeader)
+                ip = GetHeaderValueAs<string>(httpContextAccessor, "X-Forwarded-For").SplitCsv().FirstOrDefault();
+
+            // RemoteIpAddress is always null in DNX RC1 Update1 (bug).
+            if (ip.IsNullOrWhitespace() && httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress != null)
+                ip = httpContextAccessor.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            if (ip.IsNullOrWhitespace())
+                ip = GetHeaderValueAs<string>(httpContextAccessor, "REMOTE_ADDR");
+
+            // _httpContextAccessor.HttpContext?.Request?.Host this is the local host.
+
+            if (ip.IsNullOrWhitespace())
+                throw new Exception("Unable to determine caller's IP.");
+
+            // Remove port if on IP address
+            ip = ip.Substring(0, ip.IndexOf(":"));
+
+            return ip;
+        }
+
+        public static T GetHeaderValueAs<T>(IHttpContextAccessor httpContextAccessor, string headerName)
+        {
+            StringValues values;
+
+            if (httpContextAccessor.HttpContext?.Request?.Headers?.TryGetValue(headerName, out values) ?? false)
+            {
+                string rawValues = values.ToString();   // writes out as Csv when there are multiple.
+
+                if (!rawValues.IsNullOrWhitespace())
+                    return (T)Convert.ChangeType(values.ToString(), typeof(T));
+            }
+            return default(T);
+        }
+
+        public static List<string> SplitCsv(this string csvList, bool nullOrWhitespaceInputReturnsNull = false)
+        {
+            if (string.IsNullOrWhiteSpace(csvList))
+                return nullOrWhitespaceInputReturnsNull ? null : new List<string>();
+
+            return csvList
+                .TrimEnd(',')
+                .Split(',')
+                .AsEnumerable<string>()
+                .Select(s => s.Trim())
+                .ToList();
+        }
+
+        public static bool IsNullOrWhitespace(this string s)
+        {
+            return String.IsNullOrWhiteSpace(s);
+        }
+    }
+}
--- src/FreeGeoIPCore/FreeGeoIPClient.cs
+++ src/FreeGeoIPCore/FreeGeoIPClient.cs
@@ -12,6 +12,21 @@ namespace FreeGeoIPCore
     {
         public async Task<Location> GetLocation(string ipAddress)
         {
+            string url = "https://freegeoip.net/json/?q=" + ipAddress;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(new Uri(url));
+
+                response.EnsureSuccessStatusCode();
+                Location location = JsonConvert.DeserializeObject<Location>(await response.Content.ReadAsStringAsync());
+
+                return location;
+            }
+        }
+
+        public async Task<Location> GetLocation()
+        {
             string url = "https://freegeoip.net/json/";
 
             using (HttpClient client = new HttpClient())
